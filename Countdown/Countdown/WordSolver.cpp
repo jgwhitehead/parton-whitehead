f863e67daf48f22e3f1bfd7ce03e76c6de5223ea
jgwhitehead <acejames@msn.com>
2016-09-25 13:02:55 +0100

    added more comments and structure

--- Countdown/Countdown/WordSolver.cpp
+++ Countdown/Countdown/WordSolver.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main(){
     //get user input
     vector<string> userInput = getUserInput();
 
-    //for every word in the dictionary, check if you can make that word with the user 
+    //for every word in the dictionary, check if you can make that word with the user
+    vector<string> dictionaryWords = getDictionaryWords(); 
+}
+
+//read from a dictionary file to return a vector of words
+vector<string> getDictionaryWords(){
+
 }
 
 //get the letters of the challenge. 
@@ -20,3 +25,13 @@ vector<string> getUserInput(){
 
     //return the vector
 }
+
+//return true if you can make the given word with the given letters. 
+//Each letter can only be used once
+boolean validateWord(String word,vector<string> letters){
+//loop through each letter of the word. If that letter exists in the given array of letters
+// then carry on but remove the letter from the array so it can't be used again
+}
+
+
+
