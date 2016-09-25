442bbfa9ca99676a6ca6259012cc9553994282ac
jgwhitehead <acejames@msn.com>
2016-09-25 12:37:32 +0100

    Created framework for WordSolver

---
+++ Countdown/Countdown/WordSolver.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //get user input
+    vector<string> userInput = getUserInput();
+
+    //for every word in the dictionary, check if you can make that word with the user 
+}
+
+//get the letters of the challenge. 
+//Returns a vector of individual character strings
+vector<string> getUserInput(){
+    //read from the command line
+
+    //check that they only entered 8 or 9 letters, otherwise ask again
+
+    //turn the string input into a vector of individual letters
+
+    //return the vector
+}
